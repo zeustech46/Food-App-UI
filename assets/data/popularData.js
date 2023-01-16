f1c2688ea50a14ab399210341ef5d35f2955bf09
Hadi Shirath <hadishirath@Hadi-Shirath.local>
2023-01-16 08:41:48 +0700

    First Commit

---
+++ assets/data/popularData.js
@@ -0,0 +1,120 @@
+const popularData = [
+  {
+    id: '1',
+    image: require('../images/pizza1.png'),
+    title: 'Primavera Pizza',
+    weight: '540 gr',
+    rating: '5.0',
+    price: 3.99,
+    sizeName: 'Medium',
+    sizeNumber: 14,
+    crust: 'Thin Crush',
+    deliveryTime: 30,
+    ingredients: [
+      {
+        id: '1',
+        name: 'Ham',
+        image: require('../images/ham.png'),
+      },
+      {
+        id: '2',
+        name: 'Tomato',
+        image: require('../images/tomato.png'),
+      },
+      {
+        id: '3',
+        name: 'Cheese',
+        image: require('../images/cheese.png'),
+      },
+      {
+        id: '4',
+        name: 'Garlic',
+        image: require('../images/garlic.png'),
+      },
+    ],
+  },
+  {
+    id: '2',
+    image: require('../images/pizza2.png'),
+    title: 'Vegetarian Pizza',
+    weight: '450 gr',
+    rating: '4.0',
+    price: 5.99,
+    sizeName: 'Small',
+    sizeNumber: 10,
+    crust: 'Thick Crust',
+    deliveryTime: 40,
+    ingredients: [
+      {
+        id: '1',
+        name: 'Cheese',
+        image: require('../images/cheese.png'),
+      },
+      {
+        id: '2',
+        name: 'Garlic',
+        image: require('../images/garlic.png'),
+      },
+    ],
+  },
+  {
+    id: '3',
+    image: require('../images/pizza3.png'),
+    title: 'Pepperoni Pizza',
+    weight: '700 gr',
+    rating: '5.0',
+    price: 9.99,
+    sizeName: 'Large',
+    sizeNumber: 18,
+    crust: 'Thin Crust',
+    deliveryTime: 20,
+    ingredients: [
+      {
+        id: '1',
+        name: 'Tomato',
+        image: require('../images/tomato.png'),
+      },
+      {
+        id: '2',
+        name: 'Cheese',
+        image: require('../images/cheese.png'),
+      },
+    ],
+  },
+  {
+    id: '4',
+    image: require('../images/pizza2.png'),
+    title: 'Primavera Pizza',
+    weight: '540 gr',
+    rating: '5.0',
+    price: 3.99,
+    sizeName: 'Medium',
+    sizeNumber: 14,
+    crust: 'Thin Crush',
+    deliveryTime: 30,
+    ingredients: [
+      {
+        id: '1',
+        name: 'Ham',
+        image: require('../images/ham.png'),
+      },
+      {
+        id: '2',
+        name: 'Tomato',
+        image: require('../images/tomato.png'),
+      },
+      {
+        id: '3',
+        name: 'Cheese',
+        image: require('../images/cheese.png'),
+      },
+      {
+        id: '4',
+        name: 'Garlic',
+        image: require('../images/garlic.png'),
+      },
+    ],
+  },
+];
+
+export default popularData;
---
+++ components/Details.js
@@ -0,0 +1,225 @@
+import * as React from 'react';
+import {
+  View,
+  Text,
+  Font,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../assets/colors/colors';
+// import popularData from '../assets/data/popularData';
+
+export default Details = ({route, navigation}) => {
+  const {item} = route.params;
+
+  const renderIngredientsItem = ({item}) => {
+    return (
+      <View
+        style={[
+          styles.ingredientsItemWrapper,
+          {
+            marginLeft: item.id == '1' ? 20 : 0,
+          },
+        ]}>
+        <Image source={item.image} style={styles.ingredientsImage} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <SafeAreaView>
+        <View style={styles.headerWrapper}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <View style={styles.headerLeft}>
+              <Feather name="chevron-left" size={12} color={colors.textDark} />
+            </View>
+          </TouchableOpacity>
+          <View style={styles.headerRight}>
+            <MaterialCommunityIcons
+              name="star"
+              size={12}
+              color={colors.white}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+
+      {/* Title */}
+      <View style={styles.titlesWrapper}>
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+
+      {/* Price */}
+      <View style={styles.priceWrapper}>
+        <Text style={styles.priceText}>${item.price}</Text>
+      </View>
+
+      {/* Pizza Info */}
+      <View style={styles.infoWrapper}>
+        <View style={styles.infoLeftWrapper}>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Size</Text>
+            <Text style={styles.infoItemText}>
+              {item.sizeName} {item.sizeNumber}"
+            </Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Crust</Text>
+            <Text style={styles.infoItemText}>{item.crust}</Text>
+          </View>
+          <View style={styles.infoItemWrapper}>
+            <Text style={styles.infoItemTitle}>Delivery in</Text>
+            <Text style={styles.infoItemText}>{item.deliveryTime} min</Text>
+          </View>
+        </View>
+        <View>
+          <Image source={item.image} style={styles.itemImage} />
+        </View>
+      </View>
+
+      {/* Ingredients */}
+      <View style={styles.ingredientsWrapper}>
+        <Text style={styles.ingredientsTitle}>Ingredients</Text>
+        <View style={styles.ingredientsListWrapper}>
+          <FlatList
+            data={item.ingredients}
+            renderItem={renderIngredientsItem}
+            keyExtractor={item => item.id}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+      </View>
+
+      {/* Button Order */}
+      <TouchableOpacity onPress={() => alert('You Order Has Been Placed!')}>
+        <View style={styles.buttonOrderWrapper}>
+          <Text style={styles.buttonOrderTitle}> Place an order ></Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = new StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  headerLeft: {
+    borderColor: colors.textLight,
+    borderWidth: 2,
+    padding: 12,
+    borderRadius: 10,
+  },
+  headerRight: {
+    backgroundColor: colors.primary,
+    padding: 14,
+    borderRadius: 10,
+  },
+  titlesWrapper: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 32,
+    color: colors.textDark,
+    width: '60%',
+  },
+  priceWrapper: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  priceText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 32,
+    color: colors.price,
+  },
+  infoWrapper: {
+    flexDirection: 'row',
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  infoWrapper: {
+    flexDirection: 'row',
+    marginTop: 30,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  infoLeftWrapper: {
+    paddingLeft: 20,
+  },
+  infoItemWrapper: {
+    marginBottom: 20,
+  },
+  infoItemTitle: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 14,
+    color: colors.textLight,
+    marginBottom: 5,
+  },
+  infoItemText: {
+    fontFamily: 'Montserrat-SemiBold',
+    fontSize: 16,
+    color: colors.textDark,
+  },
+  itemImage: {
+    marginLeft: 24,
+    resizeMode: 'contain',
+  },
+  ingredientsWrapper: {
+    marginTop: 40,
+  },
+  ingredientsTitle: {
+    paddingHorizontal: 20,
+    fontFamily: 'Montserrat-Bold',
+    fontSize: 16,
+    color: colors.textDark,
+  },
+  ingredientsListWrapper: {
+    paddingVertical: 20,
+  },
+  ingredientsItemWrapper: {
+    backgroundColor: colors.white,
+    marginRight: 15,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    borderRadius: 15,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 2,
+  },
+  ingredientsImage: {
+    resizeMode: 'contain',
+  },
+  buttonOrderWrapper: {
+    marginTop: 30,
+    marginHorizontal: 20,
+    backgroundColor: colors.primary,
+    alignItems: 'center',
+    borderRadius: 50,
+  },
+  buttonOrderTitle: {
+    fontFamily: 'Montserrat-Bold',
+    marginVertical: 20,
+  },
+});
